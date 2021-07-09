706ff622c0585ba9df2f13969dae64f536381fb0
TheClonerx <theclonerx@outlook.com>
2021-07-09 00:13:18 -0400

    Added tcx::native_handle_type
    This is void* on win32, int otherwise

---
+++ include/tcx/native/handle.hpp
@@ -0,0 +1,17 @@
+#ifndef TCX_NATIVE_HANDLE_HPP
+#define TCX_NATIVE_HANDLE_HPP
+
+#include <cstdint>
+
+namespace tcx {
+#ifdef _WIN32
+using native_handle_type = void*;
+inline static native_handle_type invalid_handle = reinterpret_cast<void*>(~static_cast<std::uintptr_t>(0));
+#else
+using native_handle_type = int;
+inline static native_handle_type invalid_handle = -1;
+#endif
+
+}
+
+#endif
