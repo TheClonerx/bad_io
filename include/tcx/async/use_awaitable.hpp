25fd9ec9d41495adfe57382da77bff0837a4bf14
TheClonerx <theclonerx@outlook.com>
2021-07-09 15:31:55 -0400

    Added the `tcx::use_awaitable` tag

---
+++ include/tcx/async/use_awaitable.hpp
@@ -0,0 +1,11 @@
+#ifndef TCX_ASYNC_USE_AWAITABLE_HPP
+#define TCX_ASYNC_USE_AWAITABLE_HPP
+
+namespace tcx {
+struct use_awaitable_t {
+    explicit constexpr use_awaitable_t() noexcept = default;
+};
+inline constexpr auto use_awaitable = use_awaitable_t {};
+}
+
+#endif
