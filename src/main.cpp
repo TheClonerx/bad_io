bb5dacf39292b07b878bb4a5ef970ef3ac6dcbd0
TheClonerx <theclonerx@outlook.com>
2021-07-10 15:13:13 -0400

    Update main.cpp to use awaitables

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,39 @@
-
 #include <array>
+
+#include <tcx/async/open.hpp>
+#include <tcx/async/read.hpp>
+#include <tcx/async/write.hpp>
+#include <tcx/awaitable.hpp>
 #include <tcx/services/ioring_service.hpp>
 #include <tcx/unsynchronized_execution_context.hpp>
 
+template <typename E>
+tcx::awaitable<void> run(E& executor)
+{
+    std::exception_ptr e;
+    tcx::native_handle_type fd = tcx::invalid_handle;
+
+    try {
+        std::array<char, 1024 * 8> buf;
+        fd = co_await tcx::async_open(executor, "CMakeLists.txt", tcx::open_mode::read, tcx::use_awaitable);
+        std::size_t bytes_read = co_await tcx::async_read(executor, fd, buf.data(), buf.size(), tcx::use_awaitable);
+        co_await tcx::async_write(executor, STDOUT_FILENO, buf.data(), bytes_read, tcx::use_awaitable);
+    } catch (...) {
+        e = std::current_exception();
+    }
+    if (fd != -1) {
+        try {
+        } catch (std::system_error&) {
+        }
+    }
+    if (e)
+        std::rethrow_exception(e);
+    co_return;
+}
+
 int main()
 {
-    std::array<char, 1024 * 8> buf;
     tcx::unsynchronized_execution_context ctx;
-    auto& ioring = ctx.use_service<tcx::ioring_service>();
-    ioring.async_open(0, "CMakeLists.txt", O_RDONLY, 0, [&ioring, &buf](int fd) {
-        if (fd < 0)
-            throw std::system_error(-fd, std::system_category(), "opening");
-        ioring.async_read(0, fd, buf.data(), buf.size(), [&ioring, &buf](int bytes_read) {
-            if (bytes_read < 0)
-                throw std::system_error(-bytes_read, std::system_category(), "reading");
-            ioring.async_write(0, STDOUT_FILENO, buf.data(), bytes_read, [](int bytes_written) {
-                if (bytes_written < 0)
-                    throw std::system_error(-bytes_written, std::system_category(), "writting");
-            });
-        });
-    });
+    run(ctx).post_into(ctx);
     ctx.run();
 }
