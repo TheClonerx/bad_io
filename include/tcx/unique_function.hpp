1cdc3722d8c4f8bb12e617b66d81dd1a61779cc2
TheClonerx <theclonerx@outlook.com>
2021-07-10 15:12:32 -0400

    Remove unused include

--- include/tcx/unique_function.hpp
+++ include/tcx/unique_function.hpp
@@ -5,8 +5,6 @@
 #include <type_traits>
 #include <utility>
 
-#include <tcx/is_function_pointer.hpp>
-
 namespace tcx {
 template <typename F, typename Allocator = std::allocator<std::byte>>
 class unique_function;
