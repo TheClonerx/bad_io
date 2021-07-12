206cd557727a850a99f622261677fedd1160fdfb
TheClonerx <theclonerx@outlook.com>
2021-07-12 00:30:39 -0400

    Actually you can do async_poll_remove

--- include/tcx/services/epoll_service.hpp
+++ include/tcx/services/epoll_service.hpp
@@ -35,7 +35,6 @@ public:
         : m_handle { std::exchange(other.m_handle, tcx::invalid_handle) }
         , m_events(std::move(other.m_events))
         , m_completions(std::move(other.m_completions))
-        , m_last_id { std::exchange(other.m_last_id, 0) }
     {
     }
 
@@ -52,23 +51,31 @@ public:
     }
 
     template <tcx::epoll_completion_handler F>
-    auto async_poll_add(int fd, std::uint32_t events, F &&f)
+    void async_poll_add(int fd, std::uint32_t events, F &&f)
     {
         epoll_event event {};
-        event.data.u64 = ++m_last_id;
+        event.data.fd = fd;
         event.events = events | EPOLLONESHOT;
-        auto &completion = m_completions.emplace_back(static_cast<std::uint64_t>(event.data.u64), std::forward<F>(f));
+        auto &completion = m_completions.emplace_back(static_cast<int>(fd), 0, std::forward<F>(f));
         if (epoll_ctl(m_handle, EPOLL_CTL_ADD, fd, &event) < 0) {
             completion.error = errno;
         }
-        return event.data.u64;
     }
 
-    [[nodiscard]] int poll_remove(int fd) noexcept
+    template <tcx::epoll_completion_handler F>
+    void async_poll_remove(int fd, F &&f)
     {
+        auto &completion = m_completions.emplace_back(-1, 0, std::forward<F>(f));
         if (epoll_ctl(m_handle, EPOLL_CTL_DEL, fd, nullptr) < 0)
-            return errno;
-        return 0;
+            completion.error = errno;
+
+        auto it = std::find_if(m_completions.begin(), m_completions.end(), [fd](Completion const &completion) {
+            return completion.fd == fd;
+        });
+
+        // well, this should never happen unless someone added stuff using native_handle()
+        if (it != m_completions.end())
+            it->error = ECANCELED;
     }
 
     template <typename E>
@@ -77,32 +84,34 @@ public:
         if (m_completions.empty())
             return;
 
-        sigset_t prev {};
-        m_events.resize(m_completions.size());
-        int result = epoll_pwait(m_handle, m_events.data(), m_events.size(), 0 - should_block, &prev);
-        if (result < 0)
-            throw std::system_error(errno, std::system_category(), "epoll_pwait");
         for (auto &completion : m_completions) {
             if (!completion.error)
                 continue;
             executor.post([completion = std::move(completion.func), error = completion.error]() mutable {
                 completion(std::move(error), 0);
             });
-            completion.id = 0;
+            completion.fd = -1;
         }
+
+        sigset_t prev {};
+        m_events.resize(m_completions.size());
+        int result = epoll_pwait(m_handle, m_events.data(), m_events.size(), 0 - should_block, &prev);
+        if (result < 0)
+            throw std::system_error(errno, std::system_category(), "epoll_pwait");
+
         for (int i = 0; i < result; ++i) {
-            auto it = std::lower_bound(m_completions.begin(), m_completions.end(), m_events[i].data.u64, [](Completion const &completion, std::uint64_t id) {
-                return completion.id < id;
+            auto it = std::find_if(m_completions.begin(), m_completions.end(), [fd = m_events[i].data.fd](Completion const &completion) {
+                return completion.fd == fd;
             });
-            if (it != m_completions.end() && it->id == m_events[i].data.u64) {
+            if (it != m_completions.end()) {
                 executor.post([completion = std::move(it->func), events = m_events[i].events]() mutable {
                     completion(0, std::move(events));
                 });
-                it->id = 0;
+                it->fd = -1;
             }
         }
         m_completions.erase(std::remove_if(m_completions.begin(), m_completions.end(), [](Completion const &completion) {
-            return completion.id == 0 || !static_cast<bool>(completion.func);
+            return completion.fd == -1 || !static_cast<bool>(completion.func);
         }),
             m_completions.end());
     }
@@ -113,7 +122,6 @@ public:
         swap(m_handle, other.m_handle);
         swap(m_events, other.m_events);
         swap(m_completions, other.m_completions);
-        swap(m_last_id, other.m_last_id);
     }
 
     friend void swap(epoll_service &first, epoll_service &second) noexcept
@@ -133,12 +141,11 @@ private:
     tcx::native_handle_type m_handle = tcx::invalid_handle;
     std::vector<epoll_event> m_events;
     struct Completion {
-        std::uint64_t id;
+        int fd = -1;
+        int error = 0;
         tcx::unique_function<void(std::int32_t, std::uint32_t)> func;
-        std::int32_t error = 0;
     };
     std::vector<Completion> m_completions;
-    std::uint64_t m_last_id {};
 };
 
 }
