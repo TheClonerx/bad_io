f3fb2f29b56006920c82b9a520dba73114464725
TheClonerx <theclonerx@outlook.com>
2021-09-16 20:52:00 -0300

    Remove unused includes

--- include/tcx/unsynchronized_execution_context.hpp
+++ include/tcx/unsynchronized_execution_context.hpp
@@ -1,19 +1,9 @@
 #ifndef TCX_UNSYNCHRONIZED_EXECUTION_CONTEXT_HPP
 #define TCX_UNSYNCHRONIZED_EXECUTION_CONTEXT_HPP
 
-#include <deque>
-#include <memory>
-#include <memory_resource>
 #include <queue>
-#include <stdexcept>
-#include <tuple>
-#include <type_traits>
-#include <typeindex>
-#include <unordered_map>
 
-#include <tcx/is_service.hpp>
 #include <tcx/unique_function.hpp>
-#include <tcx/unique_types.hpp>
 
 namespace tcx {
 
@@ -35,14 +25,6 @@ public:
         m_function_queue.emplace(std::forward<F>(f));
     }
 
-    template <typename F>
-    void post(F &&f) requires(std::is_invocable_v<F> && !std::is_invocable_r_v<void, F> && std::is_move_constructible_v<F>)
-    {
-        m_function_queue.emplace([f = std::forward<F>(f)]() mutable {
-            f();
-        });
-    }
-
     std::size_t run();
 
     ~unsynchronized_execution_context() = default;
@@ -50,5 +32,7 @@ public:
 private:
     std::queue<function_storage> m_function_queue;
 };
+
 }
+
 #endif
