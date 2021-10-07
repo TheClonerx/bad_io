b30fb7284b13266cbde83acd83bd4f3d1dadda99
TheClonerx <theclonerx@outlook.com>
2021-10-06 22:27:30 -0300

    Add tcx::async_read for uring

---
+++ include/tcx/async/ioring/open.hpp
@@ -0,0 +1,67 @@
+#ifndef TCX_ASYNC_IORING_OPEN_HPP
+#define TCX_ASYNC_IORING_OPEN_HPP
+
+#include <stdexcept>
+#include <system_error>
+#include <type_traits>
+
+#include <fcntl.h>
+
+#include <tcx/native/handle.hpp>
+#include <tcx/services/ioring_service.hpp>
+
+namespace tcx {
+
+template <typename E, typename F>
+void async_open(E &executor, tcx::ioring_service &service, char const *path, char const *mode, F &&f) requires(std::is_invocable_v<F, std::error_code, tcx::native_handle_type>)
+{
+    (void)executor;
+
+    int flags = 0;
+
+    bool has_plus = false, has_read = false, has_write = false, has_append = false, has_cloexec = false, has_exclusive = false;
+    for (auto *it = mode; *it && *it != ','; ++it) {
+        switch (*it) {
+        case '+':
+            has_plus = true;
+            break;
+        case 'r':
+            has_read = true;
+            break;
+        case 'w':
+            has_write = true;
+            break;
+        case 'a':
+            has_append = true;
+            break;
+        case 'e':
+            has_cloexec = true;
+            break;
+        case 'x':
+            has_exclusive = true;
+            break;
+        default:
+            break;
+        }
+    }
+
+    if (has_read + has_write + has_append != 1)
+        throw std::invalid_argument("Invalid mode was provided");
+
+    if (has_plus)
+        flags |= (O_RDWR | 0) * has_read | (O_RDWR | O_CREAT | O_TRUNC) * has_write | (O_RDWR | O_CREAT | O_APPEND) * has_append;
+    else
+        flags |= (O_RDONLY | 0) * has_read | (O_WRONLY | O_CREAT | O_TRUNC) * has_write | (O_WRONLY | O_CREAT | O_APPEND) * has_append;
+    flags |= O_CLOEXEC * has_cloexec | O_EXCL * has_exclusive;
+
+    service.async_open(path, flags, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH, [f = std::forward<F>(f)](std::int32_t result) mutable -> decltype(auto) {
+        if (result < 0)
+            return f(std::error_code { -result, std::system_category() }, tcx::invalid_handle);
+        else
+            return f(std::error_code {}, result);
+    });
+}
+
+}
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
 
+#include <cstdio>
 #include <iostream>
 
+#include <system_error>
+#include <unistd.h> // ::close
+
+#include <tcx/async/ioring/open.hpp>
 #include <tcx/is_service.hpp>
 #include <tcx/services/ioring_service.hpp>
 #include <tcx/unsynchronized_execution_context.hpp>
@@ -11,5 +16,19 @@ int main()
     tcx::ioring_service io_service;
     static_assert(tcx::is_service_v<tcx::ioring_service, tcx::unsynchronized_execution_context>);
 
-    ctx.run();
+    tcx::async_open(ctx, io_service, "/etc/os-release", "rb", [](std::error_code ec, int fd) mutable {
+        if (ec)
+            throw std::system_error(ec);
+        char tmp_buf[1024] {};
+
+        auto sz = read(fd, tmp_buf, sizeof(tmp_buf));
+        if (sz < 0)
+            throw std::system_error(errno, std::system_category());
+        std::printf("%.*s\n", static_cast<int>(sz), +tmp_buf);
+        close(fd);
+    });
+
+    do {
+        io_service.poll(ctx);
+    } while (ctx.run());
 }
