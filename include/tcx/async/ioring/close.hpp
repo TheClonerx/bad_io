4706efe176ed05f5765f171accdca2d14b0e6385
TheClonerx <theclonerx@outlook.com>
2021-10-12 16:18:19 -0300

    Add tcx::async_close for io_uring

---
+++ include/tcx/async/ioring/close.hpp
@@ -0,0 +1,23 @@
+
+#ifndef TCX_ASYNC_IORING_CLOSE_HPP
+
+#include <tcx/native/handle.hpp>
+#include <tcx/services/ioring_service.hpp>
+
+namespace tcx {
+
+template <typename E, typename F>
+void async_close(E &executor, tcx::ioring_service &service, tcx::native_handle_type fd, F &&f) requires(std::is_invocable_v<F, std::error_code>)
+{
+    (void)executor;
+
+    service.async_close(fd, [f = std::forward<F>(f)](std::int32_t result) mutable {
+        if (result < 0)
+            f(std::error_code { -result, std::system_category() });
+        else
+            f(std::error_code {});
+    });
+}
+}
+
+#endif
