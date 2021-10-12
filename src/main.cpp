9bb4b58f4179f1d0f13f74e1012fbec7cdaab0a9
TheClonerx <theclonerx@outlook.com>
2021-10-12 16:19:07 -0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,10 @@
 #include <iostream>
 
 #include <system_error>
-#include <unistd.h> // ::close
 
+#include <tcx/async/ioring/close.hpp>
 #include <tcx/async/ioring/open.hpp>
+#include <tcx/async/ioring/read.hpp>
 #include <tcx/is_service.hpp>
 #include <tcx/services/ioring_service.hpp>
 #include <tcx/unsynchronized_execution_context.hpp>
@@ -16,16 +17,21 @@ int main()
     tcx::ioring_service io_service;
     static_assert(tcx::is_service_v<tcx::ioring_service, tcx::unsynchronized_execution_context>);
 
-    tcx::async_open(ctx, io_service, "/etc/os-release", "rb", [](std::error_code ec, int fd) mutable {
+    char tmp_buf[1024] {};
+
+    tcx::async_open(ctx, io_service, "/etc/os-release", "rb", [&ctx, &io_service, &tmp_buf](std::error_code ec, int fd) mutable {
         if (ec)
             throw std::system_error(ec);
-        char tmp_buf[1024] {};
 
-        auto sz = read(fd, tmp_buf, sizeof(tmp_buf));
-        if (sz < 0)
-            throw std::system_error(errno, std::system_category());
-        std::printf("%.*s\n", static_cast<int>(sz), +tmp_buf);
-        close(fd);
+        tcx::async_read(ctx, io_service, fd, +tmp_buf, sizeof(tmp_buf), [&ctx, &io_service, fd, &tmp_buf](std::error_code ec, std::size_t bytes_read) {
+            if (ec)
+                throw std::system_error(ec);
+            std::printf("%.*s\n", static_cast<int>(bytes_read), +tmp_buf);
+
+            tcx::async_close(ctx, io_service, fd, [](std::error_code ec) { 
+                if (ec)
+            throw std::system_error(ec); });
+        });
     });
 
     do {
