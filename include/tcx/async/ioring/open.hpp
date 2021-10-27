47738b5f5e43193d80f2aa41a9b0c373753ffb97
TheClonerx <theclonerx@outlook.com>
2021-10-27 17:15:13 -0300

    Make tcx::async_open use customizations

--- include/tcx/async/ioring/open.hpp
+++ include/tcx/async/ioring/open.hpp
@@ -7,26 +7,45 @@
 
 #include <fcntl.h>
 
+#include <tcx/async/concepts.hpp>
 #include <tcx/native/handle.hpp>
 #include <tcx/services/ioring_service.hpp>
 
 namespace tcx {
 
-template <typename E, typename F>
-void async_open(E &executor, tcx::ioring_service &service, char const *path, int flags, mode_t mode, F &&f) requires(std::is_invocable_v<F, std::error_code, tcx::native_handle_type>)
-{
-    service.async_open(path, flags, mode, [&executor, f = std::forward<F>(f)](std::int32_t result) mutable {
-        executor.post([f = std::move(f), result]() mutable {
-            if (result < 0)
-                f(std::error_code { -result, std::system_category() }, tcx::invalid_handle);
-            else
-                f(std::error_code {}, result);
+namespace impl {
+    template <typename E, typename F>
+    void async_open(E &executor, tcx::ioring_service &service, char const *path, int flags, mode_t mode, F &&f)
+    {
+        service.async_open(path, flags, mode, [&executor, f = std::forward<F>(f)](std::int32_t result) mutable {
+            executor.post([f = std::move(f), result]() mutable {
+                if (result < 0)
+                    f(std::error_code { -result, std::system_category() }, tcx::invalid_handle);
+                else
+                    f(std::error_code {}, result);
+            });
         });
-    });
+    }
 }
 
 template <typename E, typename F>
-auto async_open(E &executor, tcx::ioring_service &service, char const *path, char const *mode, F &&f) requires(std::is_invocable_v<F, std::error_code, tcx::native_handle_type>)
+requires tcx::completion_handler<F, tcx::native_handle_type>
+auto async_open(E &executor, tcx::ioring_service &service, char const *path, int flags, mode_t mode, F &&f)
+{
+    if constexpr (tcx::impl::has_async_transform_t<F, tcx::native_handle_type>::value) {
+        return async_open(executor, service, path, flags, mode, f.template async_transform<tcx::native_handle_type>());
+    } else if constexpr (tcx::impl::has_async_result<F>) {
+        auto result = f.async_result();
+        impl::async_open(executor, service, path, flags, mode, std::forward<F>(f));
+        return result;
+    } else {
+        impl::async_open(executor, service, path, flags, mode, std::forward<F>(f));
+    }
+}
+
+template <typename E, typename F>
+requires tcx::completion_handler<F, tcx::native_handle_type>
+auto async_open(E &executor, tcx::ioring_service &service, char const *path, char const *mode, F &&f)
 {
     bool has_plus = false, has_read = false, has_write = false, has_append = false, has_cloexec = false, has_exclusive = false;
     for (auto *it = mode; *it && *it != ','; ++it) {
@@ -50,6 +69,7 @@ auto async_open(E &executor, tcx::ioring_service &service, char const *path, cha
             has_exclusive = true;
             break;
         default:
+            /* unkown modes are ignored */
             break;
         }
     }
@@ -57,14 +77,13 @@ auto async_open(E &executor, tcx::ioring_service &service, char const *path, cha
     if (has_read + has_write + has_append != 1)
         throw std::invalid_argument("Invalid mode was provided");
 
-    int flags = 0;
+    int flags = O_CLOEXEC * has_cloexec | O_EXCL * has_exclusive;
     if (has_plus)
         flags |= (O_RDWR | 0) * has_read | (O_RDWR | O_CREAT | O_TRUNC) * has_write | (O_RDWR | O_CREAT | O_APPEND) * has_append;
     else
         flags |= (O_RDONLY | 0) * has_read | (O_WRONLY | O_CREAT | O_TRUNC) * has_write | (O_WRONLY | O_CREAT | O_APPEND) * has_append;
-    flags |= O_CLOEXEC * has_cloexec | O_EXCL * has_exclusive;
 
-    return async_open(executor, service, path, flags, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH, std::forward<F>(f));
+    return async_open(executor, service, path, flags, DEFFILEMODE, std::forward<F>(f));
 }
 
 }
