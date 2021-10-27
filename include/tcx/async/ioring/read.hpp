0d4cad6c8dadd3548a979ad8c0be746589a936ec
TheClonerx <theclonerx@outlook.com>
2021-10-27 14:12:54 -0300

    Move conditional inside the executor in async_read

--- include/tcx/async/ioring/read.hpp
+++ include/tcx/async/ioring/read.hpp
@@ -10,14 +10,12 @@ template <typename E, typename F>
 void async_read(E &executor, tcx::ioring_service &service, tcx::native_handle_type fd, void *buf, std::size_t len, F &&f) requires(std::is_invocable_v<F, std::error_code, std::size_t>)
 {
     service.async_read(fd, buf, len, -1, [&executor, f = std::forward<F>(f)](std::int32_t result) mutable {
-        if (result < 0)
-            executor.post([f = std::move(f), result]() mutable {
+        executor.post([f = std::move(f), result]() mutable {
+            if (result < 0)
                 f(std::error_code { -result, std::system_category() }, static_cast<std::size_t>(0));
-            });
-        else
-            executor.post([f = std::move(f), result]() mutable {
+            else
                 f(std::error_code {}, static_cast<std::size_t>(result));
-            });
+        });
     });
 }
 
