4dd71ef1a2929ddf65a4d98444ef454b0ad4a932
TheClonerx <theclonerx@outlook.com>
2021-10-27 14:15:37 -0300

    Add offset overload to async_read

--- include/tcx/async/ioring/read.hpp
+++ include/tcx/async/ioring/read.hpp
@@ -1,4 +1,6 @@
 
+#include <cstdio>
+#include <utility>
 #ifndef TCX_ASYNC_IORING_READ_HPP
 
 #include <tcx/native/handle.hpp>
@@ -7,9 +9,9 @@
 namespace tcx {
 
 template <typename E, typename F>
-void async_read(E &executor, tcx::ioring_service &service, tcx::native_handle_type fd, void *buf, std::size_t len, F &&f) requires(std::is_invocable_v<F, std::error_code, std::size_t>)
+void async_read(E &executor, tcx::ioring_service &service, tcx::native_handle_type fd, void *buf, std::size_t len, off_t offset, F &&f) requires(std::is_invocable_v<F, std::error_code, std::size_t>)
 {
-    service.async_read(fd, buf, len, -1, [&executor, f = std::forward<F>(f)](std::int32_t result) mutable {
+    service.async_read(fd, buf, len, offset, [&executor, f = std::forward<F>(f)](std::int32_t result) mutable {
         executor.post([f = std::move(f), result]() mutable {
             if (result < 0)
                 f(std::error_code { -result, std::system_category() }, static_cast<std::size_t>(0));
@@ -17,6 +19,12 @@ void async_read(E &executor, tcx::ioring_service &service, tcx::native_handle_ty
                 f(std::error_code {}, static_cast<std::size_t>(result));
         });
     });
+}
+
+template <typename E, typename F>
+auto async_read(E &executor, tcx::ioring_service &service, tcx::native_handle_type fd, void *buf, std::size_t len, F &&f) requires(std::is_invocable_v<F, std::error_code, std::size_t>)
+{
+    return async_read(executor, service, fd, buf, len, -1, std::forward<F>(f));
 }
 
 }
