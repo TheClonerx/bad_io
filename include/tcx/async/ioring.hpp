1968a450ad3a95423d6c52dba6e1e12ee30804dd
TheClonerx <theclonerx@outlook.com>
2021-11-04 13:59:44 -0300

    Add helper header to include all of ioring async wrappers

---
+++ include/tcx/async/ioring.hpp
@@ -0,0 +1,7 @@
+#include <tcx/async/ioring/close.hpp>
+#include <tcx/async/ioring/open.hpp>
+#include <tcx/async/ioring/poll.hpp>
+#include <tcx/async/ioring/read.hpp>
+#include <tcx/async/ioring/sleep.hpp>
+#include <tcx/async/ioring/stat.hpp>
+#include <tcx/async/ioring/write.hpp>
