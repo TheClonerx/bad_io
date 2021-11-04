ff0874a3c9ecf8b90efbdb2c152e30ea1187dfcb
TheClonerx <theclonerx@outlook.com>
2021-11-04 13:58:19 -0300

    Remove direct coroutine support for semaphore and parametrize function storage

--- include/tcx/semaphore.hpp
+++ include/tcx/semaphore.hpp
@@ -3,26 +3,22 @@
 
 #include <atomic> // std::atomic_ptrdiff_t
 #include <cassert>
-#include <coroutine>
 #include <semaphore> // std::counting_semaphore<>::max()
 #include <type_traits> // std::is_invokable_v
 #include <utility> // std::forward
 
 #include <moodycamel/concurrentqueue.h>
 
-#include <tcx/async/use_awaitable.hpp>
-#include <tcx/unique_function.hpp>
-
 namespace tcx {
 
-template <std::ptrdiff_t Max, typename E>
-class semaphore {
+template <std::ptrdiff_t Max, typename E, typename FunctionStorage>
+class basic_semaphore {
 private:
 public:
     using executor_type = E;
-    using function_storage = tcx::unique_function<void()>;
+    using function_storage = FunctionStorage;
 
-    constexpr explicit semaphore(E &executor, std::ptrdiff_t desired = Max)
+    constexpr explicit basic_semaphore(E &executor, std::ptrdiff_t desired = Max)
         : m_count(desired)
         , m_executor(executor)
     {
@@ -31,26 +27,6 @@ public:
     executor_type &executor() const noexcept
     {
         return m_executor;
-    }
-
-    auto async_acquire(tcx::use_awaitable_t)
-    {
-        struct awaiter {
-            semaphore &self;
-
-            constexpr bool await_ready() const noexcept { return false; }
-
-            void await_suspend(std::coroutine_handle<> h)
-            {
-                // might resume us right away
-                self.acquire([h = std::move(h)]() {
-                    h.resume();
-                });
-            }
-
-            constexpr void await_resume() const noexcept { }
-        };
-        return awaiter { *this };
     }
 
     template <typename F>
@@ -84,6 +60,13 @@ private:
     std::atomic_ptrdiff_t m_count;
     E &m_executor;
     moodycamel::ConcurrentQueue<function_storage> m_functions;
+};
+
+#include <tcx/unique_function.hpp>
+
+template <std::ptrdiff_t Max, typename E>
+struct semaphore : basic_semaphore<Max, E, tcx::unique_function<void()>> {
+    using basic_semaphore<Max, E, tcx::unique_function<void()>>::basic_semaphore;
 };
 
 template <typename E>
