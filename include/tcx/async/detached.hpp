8f7ed445dfba6497428c1d89fe69ce40e1b5cbdd
TheClonerx <theclonerx@outlook.com>
2021-11-13 15:15:00 -0300

    Document tcx::detached and tcx::detached_throw

--- include/tcx/async/detached.hpp
+++ include/tcx/async/detached.hpp
@@ -3,6 +3,11 @@
 
 namespace tcx {
 
+/**
+ * @brief An invocable type that accepts and ignores the arguments.
+ * @see tcx::detached
+ * @see tcx::detached_throw_t
+ */
 struct detached_t {
     explicit constexpr detached_t() noexcept = default;
 
@@ -10,6 +15,10 @@ struct detached_t {
     constexpr void operator()(Args &&...) const noexcept {};
 };
 
+/**
+ * @brief Specifies that an asynchronous operation is detached. Both the error and result are discarted.
+ * @see tcx::detached_t
+ */
 inline constexpr auto detached = detached_t {};
 
 }
--- include/tcx/async/detached_throw.hpp
+++ include/tcx/async/detached_throw.hpp
@@ -5,6 +5,13 @@
 
 namespace tcx {
 
+/**
+ * @brief An invocable type that accepts an error, and ignores the rest of the arguments.
+
+ * In case of an error, an `std::system_error` is thrown with the value of the error.
+ * @see tcx::detached_throw
+ * @see tcx::detached_t
+ */
 struct detached_throw_t {
     explicit constexpr detached_throw_t() noexcept = default;
 
@@ -16,6 +23,11 @@ struct detached_throw_t {
     };
 };
 
+/**
+ * @brief Specifies that an asynchronous operation is detached.
+ * In case of an error, an `std::system_error` is thrown with the value of the error.
+ * @see tcx::detached_throw_t
+ */
 inline constexpr auto detached_throw = detached_throw_t {};
 
 }
