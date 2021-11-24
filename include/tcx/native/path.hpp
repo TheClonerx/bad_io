6f51759eff4e81a286f8c589e7a53e03aefe4a06
TheClonerx <theclonerx@outlook.com>
2021-11-24 13:09:23 -0300

    [doc] Better alias for tcx::native_path_char_type

--- include/tcx/native/path.hpp
+++ include/tcx/native/path.hpp
@@ -2,11 +2,15 @@
 #define TCX_NATIVE_PATH_HPP
 
 namespace tcx {
-#if _WIN32
+
+#if defined(OXYGEN_INVOKED)
+using native_path_char_type = std::filesystem::path::value_type;
+#elif defined(_WIN32)
 using native_path_char_type = wchar_t;
 #else
 using native_path_char_type = char;
 #endif
+
 }
 
 #endif
