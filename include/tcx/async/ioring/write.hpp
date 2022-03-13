4024398a5f4a51846cfe8f99b3a98b2e486edecf
TheClonerx <theclonerx@outlook.com>
2022-03-13 12:19:39 -0300

    Fix missing const buf pointer in ioring_write_operation

--- include/tcx/async/ioring/write.hpp
+++ include/tcx/async/ioring/write.hpp
@@ -17,7 +17,7 @@ namespace impl {
         using result_type = std::size_t;
 
         template <typename E, typename F>
-        static void call(E &executor, tcx::ioring_service &service, tcx::native_handle_type fd, void *buf, std::size_t len, off_t offset, F &&f)
+        static void call(E &executor, tcx::ioring_service &service, tcx::native_handle_type fd, void const *buf, std::size_t len, off_t offset, F &&f)
         {
             service.async_write(fd, buf, len, offset, [&executor, f = std::forward<F>(f)](std::int32_t result) mutable {
                 executor.post([f = std::move(f), result]() mutable {
@@ -35,7 +35,7 @@ namespace impl {
  * @ingroup ioring_service
  */
 template <typename E, typename F>
-requires tcx::completion_handler<F, tcx::impl::ioring_write_operation::result_type>
+    requires tcx::completion_handler<F, tcx::impl::ioring_write_operation::result_type>
 auto async_write(E &executor, tcx::ioring_service &service, tcx::native_handle_type fd, void const *buf, std::size_t len, off_t offset, F &&f)
 {
     return tcx::impl::wrap_op<tcx::impl::ioring_write_operation>::call(executor, service, std::forward<F>(f), fd, buf, len, offset);
@@ -45,7 +45,7 @@ auto async_write(E &executor, tcx::ioring_service &service, tcx::native_handle_t
  * @ingroup ioring_service
  */
 template <typename E, typename F>
-requires tcx::completion_handler<F, tcx::impl::ioring_write_operation::result_type>
+    requires tcx::completion_handler<F, tcx::impl::ioring_write_operation::result_type>
 auto async_write(E &executor, tcx::ioring_service &service, tcx::native_handle_type fd, void const *buf, std::size_t len, F &&f)
 {
     return tcx::async_write(executor, service, fd, buf, len, -1, std::forward<F>(f));
@@ -55,7 +55,7 @@ auto async_write(E &executor, tcx::ioring_service &service, tcx::native_handle_t
  * @ingroup ioring_service
  */
 template <typename E, typename F, std::size_t Extent>
-requires tcx::completion_handler<F, tcx::impl::ioring_write_operation::result_type>
+    requires tcx::completion_handler<F, tcx::impl::ioring_write_operation::result_type>
 auto async_write(E &executor, tcx::ioring_service &service, tcx::native_handle_type fd, std::span<std::byte const, Extent> bytes, off_t offset, F &&f)
 {
     return tcx::async_write(executor, service, fd, bytes.data(), bytes.size(), offset, std::forward<F>(f));
@@ -65,7 +65,7 @@ auto async_write(E &executor, tcx::ioring_service &service, tcx::native_handle_t
  * @ingroup ioring_service
  */
 template <typename E, typename F, std::size_t Extent>
-requires tcx::completion_handler<F, tcx::impl::ioring_write_operation::result_type>
+    requires tcx::completion_handler<F, tcx::impl::ioring_write_operation::result_type>
 auto async_write(E &executor, tcx::ioring_service &service, tcx::native_handle_type fd, std::span<std::byte const, Extent> bytes, F &&f)
 {
     return tcx::async_write(executor, service, fd, bytes.data(), bytes.size(), -1, std::forward<F>(f));
