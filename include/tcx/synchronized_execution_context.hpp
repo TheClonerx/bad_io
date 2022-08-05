de0a9ac8b1d413e578dfd9a86f8f2a681e17c60e
TheClonerx <theclonerx@outlook.com>
2022-08-05 16:39:49 -0400

    Add static assert in synchronized_execution_context

--- include/tcx/synchronized_execution_context.hpp
+++ include/tcx/synchronized_execution_context.hpp
@@ -4,12 +4,14 @@
 #include <oneapi/tbb/concurrent_queue.h>
 
 #include <tcx/unique_function.hpp>
+#include <type_traits>
 
 namespace tcx {
 
 class synchronized_execution_context {
 public:
     using function_storage = tcx::unique_function<void()>;
+    static_assert(std::is_default_constructible_v<function_storage>, "function_storage type must be default constructible");
 
     synchronized_execution_context() = default;
 
@@ -33,5 +35,5 @@ private:
     oneapi::tbb::concurrent_queue<function_storage> m_function_queue;
 };
 
-}
+} // namespace tcx
 #endif
